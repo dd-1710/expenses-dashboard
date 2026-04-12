8cdd4f054a6d6d2810761f6fb47f97f1bbdca5d6
dd-1710 <ghp_4Jp3QEMskZYmgQKheNlyrn9E0QE10H0HEWKW>
2026-04-12 22:23:24 +0530

    modified path

--- FronEnd/expenses-fe/src/services/expensesService.ts
+++ FronEnd/expenses-fe/src/services/expensesService.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { catchError, Observable, throwError } from "rxjs";
 import { HttpClient } from "@angular/common/http";
-import { environment } from "../../environment";
+import { environment } from "../environments/environment";
 import { Expense } from "../interfaces/addExpense.model";
 import { BehaviorSubject } from "rxjs";
 @Injectable({
